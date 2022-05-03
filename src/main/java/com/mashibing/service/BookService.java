741847cd0508373e715442476295e801a9d16d22
xurb <xurongbiao@hmtim.com>
2022-05-03 20:50:15 +0800

    Transactional事物的参数详解

--- src/main/java/com/mashibing/service/BookService.java
+++ src/main/java/com/mashibing/service/BookService.java
@@ -3,7 +3,11 @@ package com.mashibing.service;
 import com.mashibing.dao.BookDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 @Service
 public class BookService {
@@ -11,12 +15,43 @@ public class BookService {
     @Autowired
     private BookDao bookDao;
 
-    @Transactional
-    public void buyBook(){
+
+    /**
+     * propagation:传播特性,表示不同的事物之间的关系
+     * isolation:隔离级别:4种隔离级别,会引发不同的数据错乱问题,mysql的四种隔离级别
+     * timeout:超时时间
+     * readonly:只读事物,如果配置了只读事物,那么在事物运行期间,不允许对数据进行修改,否则抛出异常
+     * noRollBackfor:设置哪些事物不回滚,(noRollbackFor = {ArithmeticException.class}),
+     * 设置哪些异常不会回滚
+     *
+     * noRollBackForClassName:设置哪些事物不回滚,
+     * @Transactional(noRollbackForClassName = {"java.lang.ArithmeticException"})
+     * RollBackfor:设置哪些异常回滚
+     * @Transactional(rollbackFor = {FileNotFoundException.class})
+     * RollBackForClassName:设置哪些异常回滚
+     *@Transactional(rollbackForClassName = {"java.io.FileNotFoundException"})
+     */
+//    @Transactional(timeout = 4)
+//    @Transactional(readOnly = true)
+    @Transactional(isolation = Isolation.DEFAULT)
+    public void buyBook() throws FileNotFoundException {
         bookDao.getprice(1);
         bookDao.updateBalance("zhangsan",100);
-        int i=1/0;
+//        try {
+//            Thread.sleep(3000);
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+        /*try{
+            int i=1/0;
+        }catch(Exception e){
+            e.printStackTrace();
+        }*/
+
         bookDao.updateStock(1);
+//        int i=1/0;
+
+        new FileInputStream("aaa.txt");
     }
 
 }
--- src/test/java/MyTest02.java
+++ src/test/java/MyTest02.java
@@ -2,12 +2,14 @@ import com.mashibing.service.BookService;
 import org.junit.Test;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.FileNotFoundException;
+
 public class MyTest02 {
 
     ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
     @Test
-    public void test01(){
+    public void test01() throws FileNotFoundException {
 
         BookService bean = context.getBean(BookService.class);
         bean.buyBook();
